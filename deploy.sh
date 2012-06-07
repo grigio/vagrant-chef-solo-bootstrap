ff8557ff69586b2b3749c1a7cba5cce5a550c76c
Charles Strahan <charles.c.strahan@gmail.com>
2012-06-07 12:08:44 -0400

    Getting somewhere...

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
- 
+
 # Usage: ./deploy.sh [host]
- 
-host="${1:-aaron@192.168.2.105}"
- 
+
+: ${1?"Usage: $0 HOST"}
+
+host="$host"
+
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
 ssh-keygen -R "${host#*@}" 2> /dev/null
- 
+
 tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
 sudo rm -rf ~/chef &&
 mkdir ~/chef &&
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
  
 # This runs as root on the server
- 
+
 chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
- 
+
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
     export DEBIAN_FRONTEND=noninteractive
@@ -12,8 +12,10 @@ if ! test -f "$chef_binary"; then
     apt-get -o Dpkg::Options::="--force-confnew" \
         --force-yes -fuy dist-upgrade &&
     # Install Ruby and Chef
-    aptitude install -y ruby1.9.1 ruby1.9.1-dev make &&
+    aptitude install -y \
+      ruby1.9.1 ruby1.9.1-dev build-essential make automake autoconf \
+      wget ssl-cert curl &&
     sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
 fi &&
- 
+
 "$chef_binary" -c solo.rb -j solo.json
